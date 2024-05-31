9c84e38724c6f3601e602070da5f64d5884ca994
Octavio Garcia <octaviogarcia1988@gmail.com>
2024-05-31 10:39:09 -0300

    refactor: Update layout and styles for homepage
    - Remove unnecessary CSS styles from style.css
    - Update the title of the homepage to "FIAP Shift Tracker with Express"
    - Add a Bootstrap jumbotron with a heading and a lead paragraph

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'FIAP Shift Tracker with Express' });
 });
 
 module.exports = router;
